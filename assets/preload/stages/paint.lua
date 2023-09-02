215e1c422403b7141568f297b5e377e9d69fa5e8
FNF-Porter-Apk-Android <143233754+FNF-Porter-Apk-Android@users.noreply.github.com>
2023-09-02 10:30:37 -0300

    Delete assets/preload/stages directory

--- assets/preload/stages/paint.lua
+++
@@ -1,8 +0,0 @@
-function onCreate()
-	makeLuaSprite('paint', 'paint', -300, 170);
-	scaleObject('paint', 2, 1.8, false)
-	setScrollFactor('paint', 0.9, 0.9);	
-	addLuaSprite('paint', false);
-	
-	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
-end
